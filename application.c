77ed0ee8420549a4a971081dfd467b1e2c5e1124
jesper9912 <75480751+jesper9912@users.noreply.github.com>
2020-12-26 18:19:11 +0100

    Update application.c

--- application.c
+++ application.c
@@ -8,7 +8,23 @@ void app_run(void)
   
    printf("0: quit\n 1: slide up\n 2: slide right\n");
    printf("3: slide down \n4: slide left\n");
+    
+    for( int y = 0;  y < 4; y++ )
+    {   
+        printf("+--------+--------+--------+--------+\n");
+        
+        printf( "|        |        |        |        |\n" );
+        printf( "|" );
+        for( int x = 0; x < 4; x++ )
+        {
+            printf( "%*d   |", 5, arr->data[x,y] );
+            
+        }
+        printf( "\n|        |        |        |        |\n" );
 
+    }
+    
+    printf("+--------+--------+--------+--------+\n");
    
 
     
